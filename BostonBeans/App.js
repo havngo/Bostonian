e7ce7c228ae2ebf543e4a6ecff33b9edbd4eaf01
allychao <chao.al@northeastern.edu>
2022-02-12 17:07:09 -0500

    login page

--- BostonBeans/App.js
+++ BostonBeans/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { render } from "react-dom";
+import { StyleSheet, Text, View } from "react-native";
+import LoginPage from "./frontend/LoginPage";
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return <LoginPage />;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
